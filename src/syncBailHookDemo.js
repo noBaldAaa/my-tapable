e4a3857b2efe57272768b55e48015006859f8d3e
wxy <102939294+noBaldAaa@users.noreply.github.com>
2022-11-06 19:32:40 +0800

    init

---
+++ src/syncBailHookDemo.js
@@ -0,0 +1,25 @@
+/**
+ * SyncBailHook是一个保险类型的同步Hook，意思是只要其中一个有返回了，后面的就不执行了
+ */
+
+const { SyncBailHook } = require("tapable");
+
+const hook = new SyncBailHook(["param1", "param2"]); //先实例化，并定义回调函数的形参
+
+//通过tap函数注册事件
+hook.tap("测试1", (param1, param2) => {
+  console.log("测试1接收的参数：", param1, param2);
+});
+
+//该监听函数有返回值
+hook.tap("测试2", (param1, param2) => {
+  console.log("测试2接收的参数：", param1, param2);
+  return "123";
+});
+
+hook.tap("测试3", (param1, param2) => {
+  console.log("测试3接收的参数：", param1, param2);
+});
+
+//通过call方法触发事件
+hook.call("hello", "world");
---
+++ src/syncHookDemo.js
@@ -0,0 +1,21 @@
+const { SyncHook } = require("tapable"); //这是一个同步钩子
+
+//第一步：实例化钩子函数，可以在这里定义形参
+const syncHook = new SyncHook(["author"]);
+
+//第二步：注册事件1
+syncHook.tap("监听器1", (name) => {
+  console.log("监听器1:", name);
+});
+
+//第二步：注册事件2
+syncHook.tap("监听器2", (name) => {
+  console.log("监听器2", name);
+});
+
+//第三步：注册事件3
+syncHook.tap("监听器3", (name) => {
+  console.log("监听器3", name);
+});
+//第三步：触发事件，这里传的是实参，会被每一个注册函数接收到
+syncHook.call("不要秃头啊");
